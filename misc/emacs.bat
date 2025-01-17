62190a2e5de24f15e03713f2e12009805079ba73
Mara Campbell <0x131181@gmail.com>
2025-01-16 23:09:28 -0800

    day 001 completed

---
+++ misc/emacs.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Emacs\emacs-29.4\bin\runemacs.exe" -q -l C:work\cpp\learning\handmade\misc\.emacs
---
+++ misc/shell.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set path=c:\work\cpp\learning\handmade\misc;%path%
+cd C:\work\cpp\learning\handmade\
+call cls
