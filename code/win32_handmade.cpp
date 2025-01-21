51d76e714b71ab82345669b097ed11782a41de72
Mara Campbell <mara@galliumstudios.com>
2025-01-20 19:48:13 -0800

    Day 004 and part of 005

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,72 +1,134 @@
 #include <Windows.h>
+#include <stdint.h>
 
 #define global        static // truly global
 #define internal      static // local to the file
 #define local_persist static // local to the scope
 
-// TODO(mara): This is a global for now
-global bool       app_is_running;
-global BITMAPINFO bitmap_info;
-global void      *bitmap_memory;
-global HBITMAP    bitmap_handle;
-global HDC        bitmap_device_context;
+// Unsigned integer types
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
-internal void Win32ResizeDIBSection(int width, int height)
+// Signed integer types
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+struct Win32OffscreenBuffer
+{
+    BITMAPINFO  info;
+    void       *memory;
+    HBITMAP     handle;
+    HDC         device_context;
+    int         width;
+    int         height;
+    int         pitch;
+    int         bytes_per_pixel;
+};
+
+// TODO(mara): These are globals for now.
+global bool                 app_is_running;
+global Win32OffscreenBuffer global_backbuffer;
+
+internal void RenderWeirdGradient(Win32OffscreenBuffer buffer, int blue_offset, int green_offset)
+{
+    // read the bitmap memory in a form we understand, not just void *
+    uint8 *row = (uint8 *)buffer.memory;
+    for (int y = 0; y < buffer.height; ++y)
+    {
+        uint32 *pixel = (uint32 *)row;
+        for (int x = 0; x < buffer.width; ++x)
+        {
+            /*
+              Memory:   BB GG RR xx
+              Register: xx RR GG BB
+
+              LITTLE ENDIAN ARCHITECTURE! REMEMBER IT'S BACKWARDS IF WE'RE DEALING WITH BYTES
+              Windows programmers wanted it to look right in the hex values, so it looks like this:
+
+              0x xxRRGGBB
+             */
+            uint8 blue = (x + blue_offset);
+            uint8 green = (y + green_offset);
+            *pixel++ = ((green << 8) | blue);
+        }
+
+        row += buffer.pitch;
+    }
+}
+
+internal void Win32ResizeDIBSection(Win32OffscreenBuffer *buffer, int width, int height)
 {
     // TODO(mara): Bulletproof this.
     // Maybe don't free first, free after, then free first if that fails.
 
-    if (bitmap_handle)
+    if (buffer->memory)
     {
-        DeleteObject(bitmap_handle);
+        VirtualFree(buffer->memory, 0, MEM_RELEASE);
     }
 
-    if (!bitmap_device_context)
-    {
-        // TODO(mara): Should we recreate these under special certain circumstances?
-        bitmap_device_context = CreateCompatibleDC(0);
-    }
+    buffer->width = width;
+    buffer->height = height;
+    buffer->bytes_per_pixel = 4;
 
-    bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
-    bitmap_info.bmiHeader.biWidth = width;
-    bitmap_info.bmiHeader.biHeight = height;
-    bitmap_info.bmiHeader.biPlanes = 1;
-    bitmap_info.bmiHeader.biBitCount = 32; // 8 bits for each RGB, but extra is for DWORD-alignment.
-    bitmap_info.bmiHeader.biCompression = BI_RGB;
+    buffer->info.bmiHeader.biSize = sizeof(buffer->info.bmiHeader);
+    buffer->info.bmiHeader.biWidth = buffer->width;
+    buffer->info.bmiHeader.biHeight = -buffer->height;
+    buffer->info.bmiHeader.biPlanes = 1;
+    buffer->info.bmiHeader.biBitCount = 32; // 8 bits for each RGB, padding for DWORD-alignment.
+    buffer->info.bmiHeader.biCompression = BI_RGB;
 
-    bitmap_handle = CreateDIBSection(bitmap_device_context, 
-                                     &bitmap_info,
-                                     DIB_RGB_COLORS, 
-                                     &bitmap_memory,
-                                     0, 0);
+    // No more DC for us.
+    int bitmap_memory_size = (buffer->width * buffer->height) * buffer->bytes_per_pixel;
+
+    buffer->memory = VirtualAlloc(0, bitmap_memory_size, MEM_COMMIT, PAGE_READWRITE);
+
+    // TODO(mara): Clear this to black.
+
+    buffer->pitch = width * buffer->bytes_per_pixel;
 }
 
-internal void Win32UpdateWindow(HDC device_context, int x, int y, int width, int height)
-{   
-    StretchDIBits(device_context, 
-                  x, y, width, height, 
-                  x, y, width, height, 
-                  bitmap_memory, 
-                  &bitmap_info,
+internal void Win32CopyBufferToWindow(HDC device_context,
+                                      RECT client_rect,
+                                      Win32OffscreenBuffer buffer,
+                                      int x,
+                                      int y,
+                                      int width,
+                                      int height)
+{
+    int window_width = client_rect.right - client_rect.left;
+    int window_height = client_rect.bottom - client_rect.top;
+    StretchDIBits(device_context,
+                  /*
+                    x, y, width, height,
+                    x, y, width, height,
+                  */
+                  0, 0, buffer.width, buffer.height,
+                  0, 0, window_width, window_height,
+                  buffer.memory,
+                  &buffer.info,
                   DIB_RGB_COLORS, SRCCOPY);
 }
 
-LRESULT CALLBACK Win32MainWindowCallback(HWND Window,
-                                    UINT Message,
-                                    WPARAM WParam,
-                                    LPARAM LParam)
+LRESULT CALLBACK Win32MainWindowCallback(HWND window,
+                                         UINT message,
+                                         WPARAM w_param,
+                                         LPARAM l_param)
 {
     LRESULT result = 0; // Set to 0 and assume that we handled all the messages that we cared about.
 
-    switch (Message)
+    switch (message)
     {
         case WM_SIZE:
         {
             RECT client_rect;
-            GetClientRect(Window, &client_rect);
+            GetClientRect(window, &client_rect);
             int width = client_rect.right - client_rect.left;
             int height = client_rect.bottom - client_rect.top;
-            Win32ResizeDIBSection(width, height);
+            Win32ResizeDIBSection(&global_backbuffer, width, height);
             OutputDebugStringA("WM_SIZE\n");
         } break;
 
@@ -91,18 +153,24 @@ LRESULT CALLBACK Win32MainWindowCallback(HWND Window,
         {
             PAINTSTRUCT paint;
 
-            HDC device_context = BeginPaint(Window, &paint);
+            HDC device_context = BeginPaint(window, &paint);
             int x = paint.rcPaint.left;
             int y = paint.rcPaint.top;
             int width = paint.rcPaint.right - paint.rcPaint.left;
             int height = paint.rcPaint.bottom - paint.rcPaint.top;
-            Win32UpdateWindow(device_context, x, y, width, height);
-            EndPaint(Window, &paint);
+
+            RECT client_rect;
+            GetClientRect(window, &client_rect);
+
+            Win32CopyBufferToWindow(device_context, client_rect,
+                              global_backbuffer,
+                              x, y, width, height);
+            EndPaint(window, &paint);
         } break;
 
         default:
         {
-            result = DefWindowProc(Window, Message, WParam, LParam);
+            result = DefWindowProc(window, message, w_param, l_param);
             // OutputDebugStringA("default\n");
         } break;
     }
@@ -116,14 +184,14 @@ int CALLBACK WinMain(HINSTANCE Instance,
                      int ShowCode)
 {
     WNDCLASS window_class = {};
-    window_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    window_class.style = CS_HREDRAW | CS_VREDRAW;
     window_class.lpfnWndProc = Win32MainWindowCallback;
     window_class.hInstance = Instance;
     window_class.lpszClassName = "HandmadeHeroWindowClass";
 
     if (RegisterClass(&window_class))
     {
-        HWND window_handle = CreateWindowEx(0, 
+        HWND window = CreateWindowEx(0,
                                             window_class.lpszClassName,
                                             "Handmade Hero",
                                             WS_OVERLAPPEDWINDOW | WS_VISIBLE,
@@ -136,22 +204,38 @@ int CALLBACK WinMain(HINSTANCE Instance,
                                             Instance,
                                             0);
 
-        if (window_handle)
+        if (window)
         {
+            int x_offset = 0;
+            int y_offset = 0;
+
             app_is_running = true;
             while (app_is_running)
             {
                 MSG message;
-                BOOL message_result = GetMessage(&message, 0, 0, 0);
-                if (message_result > 0)
+                while (PeekMessage(&message, 0, 0, 0, PM_REMOVE))
                 {
+                    if (message.message == WM_QUIT)
+                    {
+                        app_is_running = false;
+                    }
                     TranslateMessage(&message);
                     DispatchMessage(&message);
                 }
-                else
-                {
-                    break;
-                }
+
+                RenderWeirdGradient(global_backbuffer, x_offset, y_offset);
+
+                HDC device_context = GetDC(window);
+                RECT client_rect;
+                GetClientRect(window, &client_rect);
+                int window_width = client_rect.right - client_rect.left;
+                int window_height = client_rect.bottom - client_rect.top;
+                Win32CopyBufferToWindow(device_context, client_rect,
+                                        global_backbuffer,
+                                        0, 0, window_width, window_height);
+                ReleaseDC(window, device_context);
+
+                ++x_offset;
             }
         }
         else
